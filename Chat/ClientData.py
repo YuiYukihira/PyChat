881b2dce59e016db354eb8dca3c97aa681c0cbd2
YuiYukihira <littlespec200000@gmail.com>
2015-09-06 02:35:00 +0100

    Created a GUI for setting a name.
    Created a class called Namer which allows the user to set their name via
    a GUI. Added the mainloop() command to the chat GUI which I forgot to
    do.

--- Chat/ClientData.py
+++ Chat/ClientData.py
@@ -3,7 +3,33 @@ from Crypto.Cipher import AES
 from tkinter import *, ttk
 from time import sleep
 
+class Namer(threading.Thread):
+    def __init__(self, *args):
+        threading.Thread.__init__(self)
 
+    def Quit(self):
+        self.root.destroy()
+
+    def run(self):
+        self.root = Tk()
+        self.root.title('The safe python chat')
+
+        self.mainframe = ttk.Frame(self.root, padding='3 3 3 3')
+        self.mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+        self.mainframe.columnconfigure(0, weight=1)
+        self.mainframe.rowconfigure(0, weight=1)
+
+        self.name = StringVar()
+
+        ttk.Button(self.mainframe, text='Confirm name:', command=self.Setname).grid(column=1, row=1, sticky=(N, W, E, S))
+        Name_entry = ttk.Entry(self.mainframe, width=12, textVariable=self.name).grid(column=2, row=1, sticky=(N, W, E, S))
+
+        for child in self.mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+
+        self.root.bind('<Return>', Setname)
+
+        self.root.mainloop()
+        
 class GetMessage(threading.Thread):
     def __init__(self, *args):
         threading.Thread.__init__(self)
@@ -41,11 +67,13 @@ class GUI(threading.Thread):
         ttk.button(self.mainframe, text='QUIT', command=self.Quit).grid(column=2, row=1, Sticky=(N, W, E))
 
         ttk.button(self.mainframe, text='Send Message:', command=self.SendMessage).grid(column=1, row=2, sticky=(N, W, E, S))
-        Msg_entry = ttk.Entry(self.mainframe, width=20, textvariable=Msg).grid(column=2, row=2, stick=(W, E, S))
+        self.Msg_entry = ttk.Entry(self.mainframe, width=20, textvariable=Msg).grid(column=2, row=2, stick=(W, E, S))
 
         for child in self.mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
 
         self.root.bind('<Return>', self.SendMessage)
+
+        self.root.mainloop()
 
     def SendMessage():
         toSend = self.Msg.get()
