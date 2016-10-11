71a403c671c4ae7b9b2e8b15e2358cca265e3015
Ben Simms <bsimms.simms@gmail.com>
2016-10-11 11:08:56 +0100

    Added server broadcast feature
    Fixed most exceptions
    Clients referenced by socket not name
    Server broadcasts over UDP 255.255.255.255
    Moved client removal to single function

--- Chat/chatserver.py
+++ Chat/chatserver.py
@@ -1,5 +1,6 @@
-import socket, threading
-
+import socket
+import threading
+import time
 
 def GetName(c):
     SendData(c, 'NameTime')
@@ -7,21 +8,24 @@ def GetName(c):
     SendData(c, name)
     return name
 
-
-def SendData(c, data):
-    try:
-        c.send(data.encode('utf-8'))
-    except ConnectionResetError:
+def delClient(c):
         ThreadLock1.acquire()
         ThreadLock2.acquire()
-        for i in Clients:
+        for i,b in enumerate(Clients):
             if Clients[i][1] == c:
                 del Clients[i]
+                print('dropped client: {}'.format(c))
                 break
         c.close()
         ThreadLock1.release()
         ThreadLock2.release()
         
+
+def SendData(c, data):
+    try:
+        c.send(data.encode('utf-8'))
+    except ConnectionResetError:
+        delClient(c)
 
 
 def RecvData(c, buffer):
@@ -31,7 +35,7 @@ def RecvData(c, buffer):
         data = c.recv(buffer).decode('utf-8')
         return data
     except (ConnectionResetError, OSError):
-        pass
+        delClient(c)
 
 
 
@@ -47,8 +51,7 @@ class GetConnections(threading.Thread):
             s.listen(1)
             self.c, self.addr = s.accept()
             self.name = GetName(self.c)
-            Clients[self.name] = [False, self.c]
-
+            Clients.append([False, self.c, self.name, {}])
 
 class Listener(threading.Thread):
     def __init__(self, c):
@@ -61,12 +64,15 @@ class Listener(threading.Thread):
             self.text = RecvData(self.c, 1024)
             if self.text:
                 self.posted = False
+                print('Data from {}: {}'.format(self.c, self.text))
+
             else:
-                for i in Clients:
+                for i,b in enumerate(Clients):
                     if Clients[i][1] == self.c:
                         del Clients[i]
                         break
                 self.c.close()
+                print('removed client: {}'.format(self.c))
                 break
 
 class CreateListeners(threading.Thread):
@@ -80,7 +86,7 @@ class CreateListeners(threading.Thread):
             try:
                 ThreadLock1.acquire()
                 ThreadLock2.acquire()
-                for i in Clients:
+                for i,b in enumerate(Clients):
                     if Clients[i][0] == False:
                         Clients[i][0] = True
                         self.c['user'] = Clients[i][1]
@@ -92,6 +98,7 @@ class CreateListeners(threading.Thread):
             except RuntimeError:
                 print('Dictionary changed. at CreateListeners')
                 self.run()
+                break
             finally:
                 ThreadLock1.release()
                 ThreadLock2.release()
@@ -111,7 +118,7 @@ class PostMessages(threading.Thread):
                     if Listeners[i].posted == False:
                         text = Listeners[i].text
                         Listeners[i].posted = True
-                for i in Clients:
+                for i,b in enumerate(Clients):
                     if text != '':
                         if len(text) < 1024:
                             SendData(Clients[i][1], text)
@@ -121,27 +128,51 @@ class PostMessages(threading.Thread):
             finally:
                 ThreadLock1.release()
 
+class sendBroadcast(threading.Thread):
+    def __init__(self, ip, port, sname):
+        threading.Thread.__init__(self)
+        
+        self.ip = ip
+        self.port = port
+        self.sname = sname
+        
+        self.sendSock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sendSock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sendSock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    def run(self):
+        while True:
+            self.sendSock.sendto(self.sname.encode('utf-8'), (self.ip, self.port))
+            time.sleep(1)
+    
+
 
 def Main():
     kill_list = []
     GConns1 = GetConnections()
     CL1 = CreateListeners()
     PM1 = PostMessages()
+    BR1 = sendBroadcast(brhost, port, serverName)
     GConns1.start()
     CL1.start()
     PM1.start()
+    BR1.start()
 
 
 if __name__ == '__main__':
     text = ''
+    serverName = 'The Pychat server'
     Listeners = {}
     host = "0.0.0.0"
-    port = 5000
-    Clients = {}
+    brhost = "255.255.255.255"
+    port = 42120
+    Clients = []
     ThreadLock1 = threading.Lock()
     ThreadLock2 = threading.Lock()
     ThreadLock3 = threading.Lock()
     s = socket.socket()
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind((host, port))
     Main()
+
 
